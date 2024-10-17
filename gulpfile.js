ed55ad48d456fca6203094475b53f811b6fe6609
Valentindrap <valentindrap01@gmail.com>
2024-10-16 22:50:02 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, watch,  dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+
+function css(){
+    return src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('build/css'))
+}
+
+function ver(){
+    watch('src/scss/app.scss', css)
+}
+
+exports.css = css
+exports.ver = ver
